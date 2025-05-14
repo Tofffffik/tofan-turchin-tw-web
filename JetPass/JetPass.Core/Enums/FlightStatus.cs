8de2070afd26267580c7eeff0cb5da47fd4641bf
Nekit Turkin <yenadam984@kindomd.com>
2025-05-14 12:35:24 +0300

    Create FlightStatus.cs

---
+++ JetPass/JetPass.Core/Enums/FlightStatus.cs
@@ -0,0 +1,15 @@
+namespace JetPass.Core.Enums
+{
+    public enum FlightStatus
+    {
+        OnSchedule,
+        Delayed,         
+        Cancelled,      
+        Boarding,       
+        Departed,     
+        Landed,          
+        Diverted,    
+        Unknown,
+        Completed
+    }
+}
